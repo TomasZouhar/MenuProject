1773bdf18432ff02571edeaf8eca72a4f4077c91
Tomas Zouhar <xzouha13@vutbr.cz>
2024-03-14 12:28:16 +0100

    Feat: basic Firebase setup

---
+++ MenuProject/Group.swift
@@ -0,0 +1,13 @@
+//
+//  Group.swift
+//  MenuProject
+//
+//  Created by Tomáš Zouhar on 14.03.2024.
+//
+
+import Foundation
+
+struct Group: Identifiable {
+    var id: String
+    var name: String
+}
