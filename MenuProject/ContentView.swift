20e97b91b3383d9d924be66993d7d9d348f62184
Tomas Zouhar <xzouha13@vutbr.cz>
2024-03-14 09:02:48 +0100

    Initial Commit

---
+++ MenuProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MenuProject
+//
+//  Created by Tomáš Zouhar on 14.03.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MenuProject/MenuProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuProjectApp.swift
+//  MenuProject
+//
+//  Created by Tomáš Zouhar on 14.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct MenuProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
