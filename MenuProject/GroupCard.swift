feae1c48ee2d1a7e26d30b003ccc1088cbe7874d
ninarybarova <ninarybarova29@gmail.com>
2024-03-21 17:46:17 +0100

    styling group detail view + added custom colors

---
+++ MenuProject/GroupCard.swift
@@ -0,0 +1,18 @@
+//
+//  GroupCard.swift
+//  MenuProject
+//
+//  Created by Nina Rybárová on 21/03/2024.
+//
+
+import SwiftUI
+
+struct GroupCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GroupCard()
+}
